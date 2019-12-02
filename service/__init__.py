e58df561f8120b901e25749fa655598508e30350
Nickthephoon <Nicholasphoon.17@ichat.sp.edu.sg>
2019-12-02 19:00:48 +0800

    if applied, this commit will update models.py and update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,12 +1,13 @@
 # app.py or app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 def index():
     return "Hello World!"
 
