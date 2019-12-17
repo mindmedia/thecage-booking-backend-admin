fab77f79cb70cef11f605d1f4cc446cf604a79d3
cxh <XINGHAN.17@ichat.sp.edu.eg>
2019-12-17 21:10:11 +0800

    Implement Flask_Migrate

--- service/__init__.py
+++ service/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
+from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
@@ -10,8 +10,9 @@ app.config.from_pyfile("config.py")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
 ma = Marshmallow(app)
+
+migrate = Migrate(app, db)
 
 
 @app.route("/")
