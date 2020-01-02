e24999510e8381ac82d3d2ec1f7467e8d939e241
cxh <XINGHAN.17@ichat.sp.edu.eg>
2020-01-02 14:09:10 +0800

    Implement password verification for update admin

--- service/routes/admin.py
+++ service/routes/admin.py
@@ -41,19 +41,22 @@ def get_admin(Id):
 # Update an Admin
 @app.route("/admin/<Id>", methods=["PUT"])
 def update_admin(Id):
-    try:
-        admin = Admin.query.get(Id)
+    admin = Admin.query.get(Id)
 
-        user_id = request.json["user_id"]
-        password = request.json["password"]
+    user_id = request.json["user_id"]
+    password = request.json["password"]
+    old_password = request.json.get("oldPassword")
 
-        admin.user_id = user_id
-        admin.password = password
+    if admin.password == old_password:
+        try:
+            admin.user_id = user_id
+            admin.password = password
 
-        db.session.commit()
-
-    except exc.IntegrityError:
-        return json.dumps({'message': "User Id '" + user_id + "' already exists"}), 400, {'ContentType': 'application/json'}
+            db.session.commit()
+        except exc.IntegrityError:
+            return json.dumps({'message': "User Id '" + user_id + "' already exists"}), 400, {'ContentType': 'application/json'}
+    else:
+        return json.dumps({'message': 'Passwords do not match'}), 400, {'ContentType': 'application/json'}
     return (json.dumps({'message': 'success'}), 200, {'ContentType': 'application/json'})
 
 
