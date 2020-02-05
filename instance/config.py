0c2ad93f9aa56295a64331a2ffd84c601c99e330
Nickthephoon <Nicholasphoon.17@ichat.sp.edu.sg>
2020-02-05 13:04:03 +0800

    update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,8 @@
-url = "***REMOVED***" # Replace url
-db = "***REMOVED***" # Replace database
-username = "***REMOVED***" # Replace username
-password = "***REMOVED***" # Replace password
-id = "7840" #Replace id
+url = "" # Fill in the website url
+db_odoo = "" # Fill in the database for the website
+username = "" # Fill in the odoo username
+password = "" # Fill in the odoo password
+admin_id = "" # Fill in the super admins id
+admin_password = "" # Fill in the super admins password
+id = "7840" 
 DEBUG = True
